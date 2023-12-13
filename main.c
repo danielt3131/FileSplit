681320b311396b6810b726c75dbfaee73e7a9a59
Daniel Thompson <danielt3131@gmail.com>
2023-12-13 18:57:59 -0500

    Calculates the input file size

--- main.c
+++ main.c
@@ -1,6 +1,15 @@
+#define _FILE_OFFSET_BITS 64
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+const size_t fileChunckSize = 1048576;
 
 int main() {
-    printf("Hello, World!\n");
+    unsigned char *buffer = (unsigned char *) malloc((1024 * 1024) * sizeof(unsigned char)); // 1 MiB buffer
+    FILE *inputFile = fopen("input", "rb");
+    fseeko(inputFile, 0, SEEK_END);
+    size_t fileSize = ftello(inputFile);
+    fseeko(inputFile, 0, SEEK_SET);
     return 0;
 }
