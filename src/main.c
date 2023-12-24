f366267a46f797ee9e086c744664de73f3df7fd7
Daniel Thompson <danielt3131@gmail.com>
2023-12-23 19:26:51 -0600

    Added check if the cli argument for the chunk size is a number

--- src/main.c
+++ src/main.c
@@ -20,6 +20,6 @@ int main (int argc, char **argv){
     if (argc > 1){
         return (selectionCLI(argc, argv));
     } else {
-        return(modeSelection());
+        return (modeSelection());
     }
 }
